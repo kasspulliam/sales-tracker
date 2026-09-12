c188141b2479da02df3f77e9d1d14f7e6203dfe3
kasspulliam <kasspulliam@gmail.com>
2026-09-11 20:21:48 -0500

    Update ProductRepository.java

--- src/main/java/com/kassidy/sales_tracker/ProductRepository.java
+++ src/main/java/com/kassidy/sales_tracker/ProductRepository.java
@@ -1,7 +1,8 @@
 package com.kassidy.sales_tracker;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+  List<Product> findByWorkspaceIdOrderByNameAsc(String workspaceId);
 }
